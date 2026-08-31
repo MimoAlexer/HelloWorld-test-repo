8b43413dd7211a348d7d4d5c97581e4829775789
Alexander <mimoalexer@gmail.com>
2026-08-31 13:34:48 +0200

    Added Shit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 String worldString = "HOLLLYYY WOOOOORLD!";
+int symbolCount = 0;
+
 foreach (var symbol in worldString)
 {
+    symbolCount++;
     Console.WriteLine(symbol);
     Console.WriteLine("Hello git! I'm learning git shit");
     Console.WriteLine("This is the Stuff implemented");
+    
 }
+
+Console.WriteLine($"Done! Printed {symbolCount} symbols.");
