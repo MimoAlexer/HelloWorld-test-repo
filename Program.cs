57be774ae0aba74111af4a043670a19e3ec91575
Alexander <mimoalexer@gmail.com>
2026-08-31 13:45:21 +0200

    Fixed both bugs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 String worldString = "HOLLLYYY WOOOOORLD!";
-int symbolCount = 0;
+double symbolCount = 0;
 
 foreach (var symbol in worldString)
 {
-    symbolCount++;
+    symbolCount = 0.1 + 0.2; // Bug = Feature
     Console.WriteLine(symbol);
     Console.WriteLine("Hello git! I'm learning git shit");
     Console.WriteLine("This is the Stuff implemented");
-    
 }
 
 Console.WriteLine($"Done! Printed {symbolCount} symbols.");
